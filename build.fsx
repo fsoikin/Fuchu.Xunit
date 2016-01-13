9c5edca44799aa76f1ccddacdd4e4b5998810a53
Fyodor Soikin <name.fa@gmail.com>
2016-01-13 18:22:34 -0500

    Add FAKE script.

---
+++ build.fsx
@@ -0,0 +1,29 @@
+#r @"packages/FAKE/tools/FakeLib.dll"
+open Fake
+
+RestorePackages()
+
+let config = getBuildParamOrDefault "Config" "Debug"
+
+Target "Build" <| fun _ ->
+  ["Fuchu.Xunit/Fuchu.Xunit.fsproj"]
+  |> MSBuild "" "Build" ["Configuration", config]
+  |> Log "Build: "
+
+Target "BuildTests" <| fun _ ->
+  ["Fuchu.Xunit.Tests/Fuchu.Xunit.Tests.fsproj"]
+  |> MSBuild "" "Build" ["Configuration", config]
+  |> Log "Build tests: "
+
+Target "Clean" <| fun _ ->
+  !! "**/*.fsproj"
+  |> MSBuild "" "Clean" ["Configuration", config]
+  |> Log "Build tests: "
+
+Target "RunTests" <| fun _ ->
+  [sprintf "Fuchu.Xunit.Tests/bin/%s/Fuchu.Xunit.Tests.dll" config]
+  |> Fake.Testing.XUnit2.xUnit2 id
+
+"Build" ==> "BuildTests" ==> "RunTests"
+
+RunTargetOrDefault "Build"
